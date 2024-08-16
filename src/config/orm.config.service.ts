c54dbf11202d7b2d69be71f2acc6cf09a2cf2f37
dlipgart <dmitriy.lipgart@ventionteams.com>
2024-08-16 15:21:36 +0300

    changed typeorm connection options

--- src/config/orm.config.service.ts
+++ src/config/orm.config.service.ts
@@ -1,20 +1,19 @@
-import {TypeOrmModuleOptions, TypeOrmOptionsFactory} from "@nestjs/typeorm";
-import {Injectable} from "@nestjs/common";
-import {join} from "path";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import { join } from 'path';
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
-    createTypeOrmOptions(): TypeOrmModuleOptions {
-        return {
-            type: 'postgres',
-            host: process.env.DB_HOST,
-            port: +process.env.DB_PORT,
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            entities: [join(__dirname, '**', '*.entity.{ts,js}')],
-            synchronize: true,
-            autoLoadEntities: true
-        };
-    }
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      url: process.env.DB_URL,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true,
+      autoLoadEntities: true,
+    };
+  }
 }
